0f8db2077bca232760485f86b24357d674d5df2b
Roger G. Coscojuela <rogilico@gmail.com>
2016-09-28 11:29:18 +0200

    Implementa tres mensajes diferentes y inhibe rotacion pantalla.
    Esta version inhibe la rotación de pantalla, mantiene siempre la orientacion
    vertical.
    Ademas, incluye un nuevo mensaje para la segunda vez que pulsas el
    boton no tocar y una imagen nueva.

--- app/src/main/java/com/exemple/profedam/aboutrobots2017/RobotActivity.java
+++ app/src/main/java/com/exemple/profedam/aboutrobots2017/RobotActivity.java
@@ -1,14 +1,27 @@
 package com.exemple.profedam.aboutrobots2017;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class RobotActivity extends AppCompatActivity implements View.OnClickListener {
 
     private int numeroVeces = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,12 @@ public class RobotActivity extends AppCompatActivity implements View.OnClickList
         Button btnNoTocar = (Button) findViewById(R.id.btnNoTocar);
         btnNoTocar.setOnClickListener(this);
 
+        ImageButton btnSalir = (ImageButton) findViewById(R.id.imageButtonSalir);
+        btnSalir.setOnClickListener(this);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /* Método que se ejecuta cuando algún botón vigilado
@@ -29,24 +48,64 @@ public class RobotActivity extends AppCompatActivity implements View.OnClickList
 
     public void onClick(View v) {
 
-        Button btnNoTocar = (Button) v;
-        TextView textView = (TextView) findViewById (R.id.textView);
-        numeroVeces++;
-               if (numeroVeces==1)
-        {
-            btnNoTocar.setText ("¡Que no me toques!!!");
-            textView.setText ("Parece ser que los humanos no son muy inteligentes");
+        if (v.getId()==R.id.btnNoTocar) {
+            Button btnNoTocar = (Button) v;
+            TextView cajaTexto = (TextView) findViewById(R.id.textView);
+            ImageView imageRob = (ImageView) findViewById(R.id.imageView);
+            numeroVeces++;
+            if (numeroVeces == 1) {
+                btnNoTocar.setText("¡Que no me toques!!!");
+                cajaTexto.setText("Parece ser que los humanos no son muy inteligentes");
+                imageRob.setImageResource(R.mipmap.robot_enfadado);
+            }
+
+            if (numeroVeces == 2) {
+                btnNoTocar.setVisibility(View.INVISIBLE);
+                cajaTexto.setText("Te lo advertí!");
+                imageRob.setImageResource(R.mipmap.robot_fumando);
+            }
+        }
+        if (v.getId() == R.id.imageButtonSalir) {
+            finish();
+
         }
 
-        if (numeroVeces==2)
-        {
-            btnNoTocar.setVisibility(View.INVISIBLE);
-        }
 
+    }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Robot Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
